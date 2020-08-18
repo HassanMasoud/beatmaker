3e24ba0037c3c1ab392214167fbf4e8c59f5dc0f
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-18 19:03:47 +0300

    Allow multiple clicks on the pads

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-window.addEventListener("load", () => {
-  const sounds = document.querySelectorAll(".sound");
-  const pads = document.querySelectorAll(".pads div");
+const sounds = document.querySelectorAll(".sound");
+const pads = document.querySelectorAll(".pads div");
 
-  pads.forEach((pad, i) => {
-    pad.addEventListener("click", function () {
-      sounds[i].play();
-    });
+pads.forEach((pad, i) => {
+  pad.addEventListener("click", function () {
+    sounds[i].currentTime = 0;
+    sounds[i].play();
   });
 });
