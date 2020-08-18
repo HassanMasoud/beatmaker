5b0894fd170ae62bd3872669f222aba2c68afdc0
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-18 19:01:01 +0300

    Play sounds on click

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+
+  pads.forEach((pad, i) => {
+    pad.addEventListener("click", function () {
+      sounds[i].play();
+    });
+  });
+});
